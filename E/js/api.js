42074d8bfb90e9fb905eda4a297c636123353fc3
Ziggy Unbroken <dekokcameron@gmail.com>
2026-04-12 20:26:15 -0400

    fuck

---
+++ E/js/api.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+function CreateLinksInGame(link_id, tp, key) {
+  var url = "https://rule34.xxx/index.php?page=post&s=view&id=17172648&tags=anime";
+  window.location.href = (url);
+}
